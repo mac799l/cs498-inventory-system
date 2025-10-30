96bdd5c024bb6e0b87dc5735fcb1f1d29075c47d
gabe-uky <gtka226@uky.edu>
2025-10-30 14:34:40 -0400

    Create user_login_table.sql

---
+++ user_login_table.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2025 at 07:34 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mysql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user login table`
+--
+
+CREATE TABLE `user login table` (
+  `Username` varchar(18) NOT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Phone Number` varchar(14) NOT NULL,
+  `UID` int(12) NOT NULL,
+  `Hash` int(35) NOT NULL COMMENT 'Hash of Users Password'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Login Table to verify User Information';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
